87e0eb58c522c730a3a47e2e59598b3828cbae1a
Rohan Keenoy <rohankeenoy@gmail.com>
2024-09-10 19:25:58 -0500

    Final version

--- phase1Proj1/main.py
+++ phase1Proj1/main.py
@@ -1,8 +1,8 @@
 import pandas as pd
-
+import os
 class UserInputsToCsv:
     #class variable----these do not change for each new instance of class
-    startProgramDisplay = "asdlk;fjlasdk;jflasdkjflaksdjfsaldk;jf;ladskjfla;skdjflkasdjfl;aksdjfasd;lkfjasd;flkjadsf;lkj"
+    startProgramDisplay = "-------------------------------------------------------------------------------------------------------------------------------\nThis program is for creating a .csv for tracking time spent in the course. \nThe user, when prompted, will create a csv with the last name, firstname, and course number in it.\n That CSV is stored in the same directory as this program.\n ------------------------------------------------------------------------------------------------------------------------------- "
     #constructor, instance vars
     def __init__(self):
         self.lastName =""
@@ -11,7 +11,7 @@ class UserInputsToCsv:
         self.dataFrame = pd.DataFrame()
     def displayStartOfProgram(self):
         print(self.startProgramDisplay)
-        input("\nPress Enter to Continue\n")
+        input("\n.....Press Enter to Continue....\n")
     def getNamesAndId(self):
         self.lastName = input("Enter your last name: ")
         self.firstName = input("Enter your first name: ")
@@ -24,15 +24,17 @@ class UserInputsToCsv:
                     isClassId = True
         
     def displayMemberVariables(self):
-        print(f"last,first,classID: {self.lastName,self.firstName,self.classId}")
+        #print(f"last,first,classID: {self.lastName,self.firstName,self.classId}")
+        pass
     
     def createDataFrame(self):
         placement = [[self.lastName, self.firstName, self.classId]]
         self.dataFrame = pd.DataFrame(placement)
         
     def exportDataFrame(self):
-        fileName = f"{self.LastName}{self.firstName}.csv"
-        self.dataFrame.to_csv(fileName)
+        fileName = f"{self.lastName}{self.firstName}.csv"
+        self.dataFrame.to_csv(fileName,index=False,header=False)
+        print(f"CSV created with {self.lastName,self.firstName} and is in the directory of the program.\n It is called {fileName} and is located at {os.getcwd()}. \n Thank you for using our csv setup tool :)")
                     
         
         

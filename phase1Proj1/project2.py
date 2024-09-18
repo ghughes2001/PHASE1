915ae70b3b53a5f047d7096f2a9b614b9f35d66d
Rohan Keenoy <rohankeenoy@gmail.com>
2024-09-17 20:20:57 -0500

    Added correct index append to dataframe

--- phase1Proj1/project2.py
+++ phase1Proj1/project2.py
@@ -12,7 +12,11 @@ class openAppend:
         self.date = 0
         self.pplInvolved = 0
         self.notes = ""
-        self.lastEntry = 0
+        self.startingRow = 0
+        self.startTime = 0
+        self.endTime = 0
+        
+        
     def getDataFrame(self):
         validFile = False
         while (validFile == False):
@@ -46,18 +50,41 @@ class openAppend:
         print(f"Shape of df is \n {self.df.shape}")
         print(f"DataFrame is:\n{self.df}")
         
-    def getLastEntry(self):
-        self.lastEntry = self.df.iloc[-1]
-        print(f"last entry on row\n{self.lastEntry}")
+    def getStartingRow(self):
+        self.startingRow = self.df.shape[0]
+        self.startingRow+= 1
+        print(f"Starting inputs on row\n{self.startingRow}")
     
-    def addDate():
-        pass
-    def addActivityCode():
-        pass
-    def addNote():
-        pass
-    def appendToDf():
-        pass
+    def addDate(self):
+        self.date = input("\n Enter a Date, no checking yet:")
+    def addActivityCode(self):
+        self.activityCode = input("\nEnter an acitvity Code, no checking yet")
+    def addNote(self):
+        self.notes = input ("\nEnter a note, no checking yet: ")
+        
+    def addStartTime(self):
+        self.startTime = input("\nadd a start time, not checked yet")
+    def addEndTime(self):
+        self.endTime = input("\nadd an end time, not checked yet")
+    
+    def addHowMany(self):
+        self.pplInvolved = input("\n How many people? not checked yet")
+        
+        
+    def appendToDf(self):
+        print(f"Data Frame Before Appending: \n {self.df}\n")
+        
+        new_row = {
+            0: self.date,
+            1: self.startTime,
+            2: self.endTime,
+            3: self.pplInvolved,
+            4: self.activityCode,
+            5: self.notes
+        }
+        self.df = self.df._append(new_row, ignore_index=True)
+        
+        print(f"Dataframe after appending:\n {self.df}\n")
         
         
     def populateDataFrame(self):
@@ -76,4 +103,11 @@ if __name__ == "__main__":
     instance.getDataFrame()
     instance.dataCheck()
     instance.printDf()
-    instance.getLastEntry()
+    instance.getStartingRow()
+    instance.addActivityCode()
+    instance.addHowMany()
+    instance.addNote()
+    instance.addStartTime()
+    instance.addEndTime()
+    instance.addDate()
+    instance.appendToDf()

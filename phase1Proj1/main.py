505375cfb708fe273089b3fc4b86dc4bcc0156b7
Rohan Keenoy <rohankeenoy@gmail.com>
2024-09-21 17:03:11 -0500

    worked on proj C and noticed an error and fixed it

--- phase1Proj1/main.py
+++ phase1Proj1/main.py
@@ -1,3 +1,20 @@
+"""
+    Language: Python 3
+    IDE: VS Code
+    HOW TO RUN: Navigate to where the python project and execute as python {projectname}.py, because python is an interpreted language, an external library is used to "compile" it for an executable.
+    Testing was ran with the python command.
+    AUTHORS: Rohan Keenoy 
+    DATA STRUCTURES: A pandas dataframe is commonly used in scientific applications, it can be thought of as a N-d array,can have headers, and is structured as the csv is structured. 
+    For this project using a dataframe is a no brainer. R has a similar structure. 
+    A dictionary is used to append to rows in a dataframe. This generated per row-entry. 
+    EXTERNAL files: Only the log file generated from program A is used.
+    External preperation: Because python is an interpreted language a software _____ will be used to generate an executable. 
+    
+    References:
+    This program is pretty easy/self explanatory for those who regularly work with csv data, no references were needed.
+"""
+
+
 import pandas as pd
 import os
 class UserInputsToCsv:
@@ -9,9 +26,11 @@ class UserInputsToCsv:
         self.firstName=""
         self.classId=""
         self.dataFrame = pd.DataFrame()
+    #displays the message for start of program
     def displayStartOfProgram(self):
         print(self.startProgramDisplay)
         input("\n.....Press Enter to Continue....\n")
+    #gets name and ID
     def getNamesAndId(self):
         self.lastName = input("Enter your last name: ")
         self.firstName = input("Enter your first name: ")
@@ -22,15 +41,16 @@ class UserInputsToCsv:
                 self.classId = input("Please enter a class ID (CS 4500): ")
                 if self.classId == "CS 4500":
                     isClassId = True
-        
+    #used for making sure member vars are proper
     def displayMemberVariables(self):
         #print(f"last,first,classID: {self.lastName,self.firstName,self.classId}")
         pass
-    
+    #creates the data frame, adds last name, first on row 1, classID on row 2.
     def createDataFrame(self):
+    
         placement = [self.lastName, self.firstName], [self.classId]
         self.dataFrame = pd.DataFrame(placement)
-        
+    #exports to current directory  
     def exportDataFrame(self):
         fileName = f"{self.lastName}{self.firstName}Log.csv"
         self.dataFrame.to_csv(fileName,index=False,header=False)
@@ -38,7 +58,7 @@ class UserInputsToCsv:
         print(f"CSV created with {self.lastName,self.firstName} and is in the directory of the program.\n It is called {fileName} and is located at {os.getcwd()}. \n Thank you for using our csv setup tool :)")
         print("\n-------------------------------------------------------------------------------------------------------------------------------" )          
         
-        
+#main driver       
 if __name__ =="__main__":
     instance = UserInputsToCsv()
     instance.displayStartOfProgram()
